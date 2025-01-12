b6e431002f523460be1ef6a4fae108574c1d13fa
raghav_28o <iamraghavagarwal28@gmail.com>
2025-01-12 19:00:48 +0530

    12 jan 25

--- src/main/java/com/project/uber/uberApp/services/impl/DistanceServiceOSRMImpl.java
+++ src/main/java/com/project/uber/uberApp/services/impl/DistanceServiceOSRMImpl.java
@@ -1,15 +1,37 @@
 package com.project.uber.uberApp.services.impl;
 
 import com.project.uber.uberApp.services.DistanceService;
+import lombok.Data;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 import java.awt.*;
+import java.util.List;
 
 @Service
 public class DistanceServiceOSRMImpl implements DistanceService {
+
+    private final String OSRM_API_BASE_URL = "http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407";
     @Override
     public double calculateDistance(Point src, Point dest) {
-        // Call 3rd party api to OSRM to fetch distanc
+        // Call 3rd party api to OSRM to fetch distance
+        OSRMResponseDto responseDto =  RestClient.builder()
+                .baseUrl(OSRM_API_BASE_URL)
+                .build()
+                .get()
+                .uri("{},{};{},{}", src.getX(),src.getY(), dest.getX(), dest.getY())
+                .retrieve()
+                .body(OSRMResponseDto.class);
         return 0;
     }
+}
+
+@Data
+class OSRMResponseDto {
+    private List<OSRMRoute> routes;
+}
+
+
+class OSRMRoute {
+    private Double distance;
 }
--- src/main/java/com/project/uber/uberApp/strategies/impl/RideFareSurgePricingFareCalculationStrategy.java
+++ src/main/java/com/project/uber/uberApp/strategies/impl/RideFareSurgePricingFareCalculationStrategy.java
@@ -1,10 +1,9 @@
 package com.project.uber.uberApp.strategies.impl;
 
-import com.project.uber.uberApp.dto.RideRequestDto;
 import com.project.uber.uberApp.entities.RideRequest;
 import com.project.uber.uberApp.strategies.RideFareCalculationStrategy;
 
-public class RideFareSurgeePricingFareCalculationStrategy implements RideFareCalculationStrategy {
+public class RideFareSurgePricingFareCalculationStrategy implements RideFareCalculationStrategy {
     @Override
     public double calculateFare(RideRequest rideRequest) {
         return 0;
