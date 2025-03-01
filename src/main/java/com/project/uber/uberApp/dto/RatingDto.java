2ab2469d006f3dc7fedf64ebed1ac59a38db25a6
raghav_28o <iamraghavagarwal28@gmail.com>
2025-03-01 17:16:11 +0530

    1 march 2025

---
+++ src/main/java/com/project/uber/uberApp/dto/RatingDto.java
@@ -0,0 +1,13 @@
+package com.project.uber.uberApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingDto {
+    private Long rideId;
+    private Integer rating;
+}
